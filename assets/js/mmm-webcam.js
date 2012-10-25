c0552ef81efe233173172551762e05febffed95a
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2012-10-25 00:33:00 -0700

    fly in animation

--- assets/js/mmm-webcam.js
+++ assets/js/mmm-webcam.js
@@ -15,6 +15,6 @@ jQuery(document).ready( function () {
     // Animation
     TweenMax.to( jQuery("h3.webcams"), 0, {css: { alpha: 0, marginLeft:600 } } );
     TweenMax.to( jQuery("h3.webcams"), 0.7, {css: { alpha: 1, marginLeft:0 }, ease: Bounce.easeOut } );
-    TweenMax.to( jQuery(".mmm-webcam"), 0, {css: { rotation:181, alpha:0 } } );
-    TweenMax.to( jQuery(".mmm-webcam"), 0.4, {css: { rotation:0, alpha:1 }, delay: 0.7, ease: Sine.easeOut } );
+    TweenMax.to( jQuery(".mmm-webcam"), 0, {css: { marginTop:"-600px", rotation:181, alpha:0 } } );
+    TweenMax.to( jQuery(".mmm-webcam"), 0.4, {css: { marginTop:"0", rotation:0, alpha:1 }, delay: 0.7, ease: Sine.easeOut } );
 });
